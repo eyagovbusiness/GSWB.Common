b9c94ca98d6b1673ea16bb0b99750a1aec26f256
Eduardo Yago Vicent <124583064+eyagovbusiness@users.noreply.github.com>
2024-03-27 21:08:25 +0100

    GSWB-58 Added EventsApiRoutes.

---
+++ src/Common.Infrastructure/Communication/ApiRoutes/EventsApiRoutes.cs
@@ -0,0 +1,10 @@
+﻿namespace Common.Infrastructure.Communication.ApiRoutes
+{
+    public struct EventsApiRoutes
+    {
+        #region Endpoints
+        public const string events_new = "events/new";
+        #endregion
+
+    }
+}
