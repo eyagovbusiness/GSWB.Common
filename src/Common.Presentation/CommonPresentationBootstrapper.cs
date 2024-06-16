98924b417ae111f125880b48d27619a6958e5e2b
Eduardo Yago Vicent <124583064+eyagovbusiness@users.noreply.github.com>
2024-06-16 23:18:10 +0200

    Added UseCommonPresentation for health endpoint and swagger configuration common to all services

--- src/Common.Presentation/CommonPresentationBootstrapper.cs
+++ src/Common.Presentation/CommonPresentationBootstrapper.cs
@@ -1,6 +1,10 @@
 ﻿using Common.Presentation.Validation;
 using FluentValidation;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+using TGF.CA.Application;
 
 namespace Common.Presentation
 {
@@ -19,5 +23,18 @@ namespace Common.Presentation
         }
 
         //declare here more common use middlewares, but be careful declaring common middleware use order becauyse the order matters! and it may not be a common order that suits all the applications of this project.
+        public static void UseCommonPresentation(this WebApplication aWebApplication)
+        {
+            if (aWebApplication.Environment.IsDevelopment() || aWebApplication.Environment.IsStaging())
+            {
+                aWebApplication.UseSwagger();
+                aWebApplication.UseSwaggerUI();
+            }
+            aWebApplication.MapHealthChecks(TGFEndpointRoutes.health, new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+        }
     }
 }
