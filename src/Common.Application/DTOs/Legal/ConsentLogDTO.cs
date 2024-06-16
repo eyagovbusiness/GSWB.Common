dae36e8ecd49659aa5097d62fc735afa9414fc1d
Eduardo Yago Vicent <124583064+eyagovbusiness@users.noreply.github.com>
2024-06-16 15:24:57 +0200

    Added Legal DTOs and legal api routes

---
+++ src/Common.Application/DTOs/Legal/ConsentLogDTO.cs
@@ -0,0 +1,7 @@
+﻿
+using Common.Domain.ValueObjects;
+
+namespace Common.Application.DTOs.Legal
+{
+    public record ConsentLogDTO(ConsentTypeEnum ConsentType, string PrivacyPolicyVersion, ConsentMethodEnum ConsentMethod, string? UserAgent, string? Geolocation);
+}
--- src/Common.Infrastructure/Communication/ApiRoutes/APIGatewayApiRoutes.cs
+++ src/Common.Infrastructure/Communication/ApiRoutes/APIGatewayApiRoutes.cs
@@ -5,37 +5,43 @@
         public struct Auth_signIn
         {
             public const string Route = "auth/sign-in";
-            public const string OperationId = "auth-sign-in";
+            public const string OperationId = "auth_sign-in";
         }
 
         public struct Auth_testerSignIn
         {
             public const string Route = "auth/tester-sign-in";
-            public const string OperationId = "auth-tester-sign-in";
+            public const string OperationId = "auth_tester-sign-in";
         }
 
         public struct Auth_signUp
         {
             public const string Route = "auth/sign-up";
-            public const string OperationId = "auth-sign-up";
+            public const string OperationId = "auth_sign-up";
         }
 
         public struct Auth_signOut
         {
             public const string Route = "auth/sign-out";
-            public const string OperationId = "auth-sign-out";
+            public const string OperationId = "auth_sign-out";
         }
 
         public struct Auth_token
         {
             public const string Route = "auth/token";
-            public const string OperationId = "auth-token";
+            public const string OperationId = "auth_token";
         }
 
         public struct Auth_token_refresh
         {
             public const string Route = "auth/token/refresh";
-            public const string OperationId = "auth-token-refresh";
+            public const string OperationId = "auth_token_refresh";
+        }
+
+        public struct Legal_consent
+        {
+            public const string Route = "legal/consent";
+            public const string OperationId = "legal_consent";
         }
     }
 }
